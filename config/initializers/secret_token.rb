4e448c6295b9548f69ef7beab06529eb079028cf
jdell <jdell@drwholdings.com>
2011-03-13 16:35:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playerpool::Application.config.secret_token = '848354a43e69b478308ff9a593d519f244771a5497924fc231f4964168eebbee49680d6218a625ddca17b83ccb7ec3be85604e7468aef6243538356dc1da2c8a'
